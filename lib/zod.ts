63cb388e66b01c5b03dac39501007119738c35bd
goliter <goliterwzh@outlook.com>
2025-03-11 14:38:10 +0800

    fix : 修复因哈希后密码长度超出最大密码长度的问题

--- lib/zod.ts
+++ lib/zod.ts
@@ -6,5 +6,4 @@ export const signInSchema = object({
     .email("Invalid email"),
   password: string({ required_error: "Password is required" })
     .min(1, "Password is required")
-    .max(32, "Password must be less than 32 characters"),
 });
